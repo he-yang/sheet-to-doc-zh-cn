be401fc028cd888cae0d30053c208b983ae4e5fa
wtsolutions <he.yang@wtsolutions.cn>
2026-08-25 10:33:31 +0800

    update free limit

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'Sheet-to-Doc 技术文档'
 copyright = '2022~2026, WTSolutions'
 author = 'WTSolutions'
-release = '2.15.0.0'
+release = '2.16.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
